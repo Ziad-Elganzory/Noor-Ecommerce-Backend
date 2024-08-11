451b43848b4fbc43cb4d7c4870a89ef69a732a93
Ziad-Elganzory <ziadmohamed770@outlook.com>
2024-08-11 12:18:09 +0300

    migrations & models

---
+++ app/Models/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'slug',
+        'images',
+        'description',
+        'price',
+        'is_active',
+        'is_featured',
+        'in_stock',
+        'on_sale'
+    ];
+
+}
